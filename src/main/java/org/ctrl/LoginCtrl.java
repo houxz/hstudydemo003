a2463ce3ec3353dc56f620b4fe104f1672f4602d
Administrator <243210740@qq.com>
2019-05-09 22:13:19 +0800

    a

--- src/main/java/org/ctrl/LoginCtrl.java
+++ src/main/java/org/ctrl/LoginCtrl.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class LoginCtrl {
 
-	@RequestMapping(value="/login")
+	@RequestMapping(value="/login.web")
 	public String  LoadPage() {
 		System.out.println("==========LoginCtrl::LoadPage()==================");
 		return "login";
